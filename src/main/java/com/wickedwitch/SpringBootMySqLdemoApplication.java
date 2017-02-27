357602ae65fa8cf4c29a3ea3e1993bedc166802e
WickedWitchWarsaw <zuzka.pacak@gmail.com>
2017-02-27 23:42:29 +0100

    Initial commit - empty demo project

---
+++ src/main/java/com/wickedwitch/SpringBootMySqLdemoApplication.java
@@ -0,0 +1,12 @@
+package com.wickedwitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMySqLdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMySqLdemoApplication.class, args);
+	}
+}
